6a5aa214a3bc1faa773a5c4ac2165b6fc4d99d8b
zqr <1546975213@QQ.COM>
2022-04-07 13:46:59 +0800

    Ready for push

---
+++ MyPlugin.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
+#pragma once
+
+#include "include/DTrackSDK.hpp"
+#include <mc_control/GlobalPlugin.h>
+#include <iostream>
+#include <sstream>
+#include <Eigen/Dense>
+#include <fstream>
+
+namespace mc_plugin
+{
+
+struct MyPlugin : public mc_control::GlobalPlugin
+{
+  void init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config) override;
+
+  void reset(mc_control::MCGlobalController & controller) override;
+
+  void before(mc_control::MCGlobalController & controller) override;
+
+  void after(mc_control::MCGlobalController & controller) override;
+
+  mc_control::GlobalPlugin::GlobalPluginConfiguration configuration() override;
+
+  ~MyPlugin() override;
+  
+  void assign(mc_control::MCGlobalController & controller);
+  
+  bool data_error_to_console();
+
+private:
+  Eigen::Vector4d loc_tar_0, loc_tar_1, Quatern_temp;
+  Eigen::Vector3d trans0, trans_body;
+  Eigen::Quaterniond rot_tar_0, rot_tar_1, rot_body, rot_T;
+  Eigen::Matrix4d T0, T_m2b, T_marker, T_body;
+  Eigen::Matrix3d R0, R_body;
+  DTrackSDK * dt;
+  char line[256];
+  int linenum = 1;
+  double intarr[4];
+  bool tracked = false;
+
+  // For data transfer: 
+  Eigen::Quaterniond rot;
+};
+
+} // namespace mc_plugin
